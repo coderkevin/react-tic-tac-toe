354c6a240bb72d1d6cdbda9e13fe4d16dff3ea18
coderkevin <kevin@redfenix.com>
2022-11-21 01:52:45 -0600

    Add emotion for css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import React from "react";
+import styled from "@emotion/styled/macro";
 import logo from "./logo.svg";
 import "./App.css";
+
+const PinkParagraph = styled.p`
+  color: hotpink;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <PinkParagraph>
           Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        </PinkParagraph>
         <a
           className="App-link"
           href="https://reactjs.org"
